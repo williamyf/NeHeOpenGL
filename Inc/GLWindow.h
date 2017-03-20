cbec51e0036ae7e90d6c5fefcd6bd3b6bc927c44
wyfhomepc@example.com <wyfhomepc@example.com>
2017-03-21 00:35:09 +0800

    新建目录结构

---
+++ Inc/GLWindow.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+BOOL CreateGLWindow(char* title, int width, int height, int bits, bool fullscreenflag);
---
+++ Src/GLWindow.cpp
@@ -0,0 +1,6 @@
+#include "../Inc/GLWindow.h"
+
+BOOL CreateGLWindow(char* title, int width, int height, int bits, bool fullscreenflag)
+{
+	return FALSE;
+}
